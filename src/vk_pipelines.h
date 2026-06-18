e0461fe45d39a94e2c71b039b573f84d0f1b5da1
Amir3022 <amir.tayie98@gmail.com>
2026-06-18 21:02:45 +0300

    Created functions in vk_pipeline to load shader module from file (path of compiled glsl shader to spirv)

--- src/vk_pipelines.h
+++ src/vk_pipelines.h
@@ -1,7 +1,7 @@
 ﻿#pragma once 
 #include <vk_types.h>
+#include <string>
 
 namespace vkutil {
-
-
+    bool load_Shader_Module(const std::string& filePath, VkDevice device, VkShaderModule* outShaderModule);
 };
